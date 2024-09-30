25dc1daab83d016e4c51686ab49facf843c3cd7a
MJJKowalski <77116266+TheFrozenBrawler@users.noreply.github.com>
2024-09-30 14:27:48 +0200

    Added Inc folder

---
+++ Inc/measure_handler.h
@@ -0,0 +1,16 @@
+/*
+ * measure_handler.h
+ *
+ *  Created on: May 12, 2024
+ *      Author: macie
+ */
+
+#include "stm32h7xx_hal.h"
+
+#ifndef SRC_MEASURE_HANDLER_H_
+#define SRC_MEASURE_HANDLER_H_
+
+uint16_t ADC_take_measure(ADC_HandleTypeDef *adc_handler);
+
+
+#endif /* SRC_MEASURE_HANDLER_H_ */
